aab202f8d08b8cd7cfe195e85f71d3cfbc22b795
Tracy Mu Sung <tracy.musung@moomumedia.com>
2017-01-20 16:09:06 +1100

    Make request to IMDB

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,21 @@
 class MoviesController < ApplicationController
   def search
+    return unless @title = params[:q]
+    make_request    
+    parse_response
+    render('movies/show')
   end
 
   def show
+  end
+
+  def make_request
+    uri = URI.parse("http://www.omdbapi.com/?s=#{ @title }")
+    http =  Net::HTTP.new(uri.host, uri.port)
+    @results = http.get(uri.request_uri)
+  end
+
+  def parse_response
+    @movies = JSON.parse(@results.body)['Search']
   end
 end
