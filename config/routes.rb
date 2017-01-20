5959b4c43723b4219247e08f9e6bd6bdcccf1f87
Tracy Mu Sung <tracy.musung@moomumedia.com>
2017-01-20 16:08:50 +1100

    Use post request

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'movies/search'
 
+  post 'movies/search'
+
   get 'movies/show'
 
   # The priority is based upon order of creation: first created -> highest priority.
