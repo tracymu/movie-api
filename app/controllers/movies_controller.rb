98901c9a204a46a474296a18b35fada93933d0e1
Geoff Hodgson <geoffroh@gmail.com>
2017-02-21 19:55:39 +1100

    Some refactoring of movies/favourites controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,21 +1,25 @@
 class MoviesController < ApplicationController
   def search
-    return unless @title = params[:q]
-    make_request    
-    parse_response
-    render('movies/show')
+    if params[:q]
+      @movies = request_movies_from_omdb
+    end
   end
 
   def show
   end
 
-  def make_request
-    uri = URI.parse("http://www.omdbapi.com/?s=#{ @title }")
-    http =  Net::HTTP.new(uri.host, uri.port)
-    @results = http.get(uri.request_uri)
+  private
+
+  def request_movies_from_omdb
+    response = http_client.get(omdb_url)
+    JSON.parse(response.body)['Search']
   end
 
-  def parse_response
-    @movies = JSON.parse(@results.body)['Search']
+  def http_client
+    Net::HTTP.new(omdb_url.host, omdb_url.port)
+  end
+
+  def omdb_url
+    @omdb_url ||= URI.parse("http://www.omdbapi.com/?s=#{params[:q]}")
   end
 end
